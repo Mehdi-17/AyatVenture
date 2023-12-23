ad3c07a82675da2ae8bf6f169c85d02e5cbb7b7d
Mehdi-17 <mehdi.serra57@gmail.com>
2023-12-23 17:25:53 +0100

    Initial commit

---
+++ src/main/java/com/ayatventure/ayatventure/AyartVentureApplication.java
@@ -0,0 +1,13 @@
+package com.ayatventure.ayatventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyartVentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyartVentureApplication.class, args);
+	}
+
+}
