102b980629f9854a6bb8bf5b18a4747fd1887aa6
Mehdi-17 <mehdi.serra57@gmail.com>
2023-12-23 17:29:35 +0100

    Refactor Name

--- src/main/java/com/ayatventure/ayatventure/AyatVentureApplication.java
+++ src/main/java/com/ayatventure/ayatventure/AyatVentureApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AyartVentureApplication {
+public class AyatVentureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AyartVentureApplication.class, args);
+		SpringApplication.run(AyatVentureApplication.class, args);
 	}
 
 }
